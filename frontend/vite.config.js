328c4a54f76cfab74f10292e6761661c2cb8512c
Viktor Taseski <vtaseski24@gmail.com>
2026-04-18 15:50:10 +0200

    changed default local port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5000,
     proxy: {
-      '/ws': {
-        target: 'ws://localhost:8000',
+      "/ws": {
+        target: "ws://localhost:8000",
         ws: true,
       },
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
       },
     },
   },
-})
+});
