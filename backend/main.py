25a68daeb0ff074c9af31b7ae3f07e937e9cbae9
FT1E <ft1hcfu@gmail.com>
2026-04-18 16:36:46 +0200

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -378,6 +378,9 @@ async def websocket_events(websocket: WebSocket):
         recording = False
 
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
