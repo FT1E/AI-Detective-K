03c0794946bf372186be032371a0fc9463997f8c
FT1E <ft1hcfu@gmail.com>
2026-04-18 22:13:06 +0200

    Added depth perception info from OAK camera

--- test_cam.py
+++ test_cam.py
@@ -2,6 +2,7 @@
 
 import cv2
 import depthai as dai
+import numpy as np
 
 # Create pipeline
 with dai.Pipeline() as pipeline:
@@ -12,12 +13,47 @@ with dai.Pipeline() as pipeline:
     cam = pipeline.create(dai.node.Camera).build()
     videoQueue = cam.requestOutput((640,400)).createOutputQueue()
 
+    # queues for depth perception
+    monoLeft = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+    monoRight = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
+    stereo = pipeline.create(dai.node.StereoDepth)
+
+    # Linking depth
+    monoLeftOut = monoLeft.requestFullResolutionOutput()
+    monoRightOut = monoRight.requestFullResolutionOutput()
+    monoLeftOut.link(stereo.left)
+    monoRightOut.link(stereo.right)
+
+    # stereo setting setup
+    stereo.setRectification(True)
+    stereo.setExtendedDisparity(True)
+    stereo.setLeftRightCheck(True)
+
+    # depth queue
+    disparityQueue = stereo.disparity.createOutputQueue()
+
+    # different color map for depth
+    colorMap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_JET)
+    colorMap[0] = [0, 0, 0]  # to make zero-disparity pixels black
+
 
     # Connect to device and start pipeline
     pipeline.start()
-    while pipeline.isRunning():
+
+    # default value
+    maxDisparity = 1
+    while pipeline.isRunning():        
         videoIn = videoQueue.get()
         assert isinstance(videoIn, dai.ImgFrame)
+
+        # get disparity / depth
+        disparity = disparityQueue.get()
+        assert isinstance(disparity, dai.ImgFrame)
+        
+        npDisparity = disparity.getFrame()
+        maxDisparity = max(maxDisparity, np.max(npDisparity))
+        colorizedDisparity = cv2.applyColorMap(((npDisparity / maxDisparity) * 255).astype(np.uint8), colorMap)
+        cv2.imshow("disparity", colorizedDisparity)
 
         videoIn.getCvFrame()        # send this to server
         cv2.imshow("video", videoIn.getCvFrame())
